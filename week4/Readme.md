264e3709f03be09a4fe29105f44690c4f719c10b
Lynn Cherny <arnicas@gmail.com>
2018-02-12 12:24:24 +0100

    week4

---
+++ week4/Readme.md
@@ -0,0 +1,62 @@
+## Topic Modeling and Entity Recognition
+
+Topics are described by words in a document.
+
+<img src="assets/Readme-e0540.png">
+
+From [David Blei article in CACM](https://cacm.acm.org/magazines/2012/4/147361-probabilistic-topic-models/fulltext):
+
+<img src="assets/Readme-a5b6b.png">
+
+Analyzing topic output in Excel:
+
+<img src="assets/Readme-333ea.png">
+
+<img src="assets/Readme-c1303.png">
+
+Looking at topics over time:
+
+From [Ben Schmidt analysis of scripts](http://sappingattention.blogspot.fr/2014/12/fundamental-plot-arcs-seen-through.html):
+
+<img src="assets/Readme-423a4.png">
+
+
+From [David Blei in CACM](https://cacm.acm.org/magazines/2012/4/147361-probabilistic-topic-models/fulltext):
+
+<img src="assets/Readme-51589.png">
+
+
+## Final Projects Datasets
+
+You must all analyse a different dataset.
+
+Your options are:
+
+* Twitter data (or Instagram data) that I have in a database for a particular search topic.
+* Collect your own Twitter data.
+* Screenplays by common writers or directors (I can help).
+* Product reviews from Amazon (I can provide, some are in a database too).
+* Make your own collection of books -- the same author, or same era / timeperiod, or some other theme.
+* Yelp data on a single business (can't be one another used), this is also in a database.
+* Other... you suggest. It has to be not tiny.
+
+Post your choice on the Discussion Forum topic I will create.
+
+The project requires 2 items:
+
+A PDF summarizing your findings, using charts, data results, text extracts that illustrate what you found.  You should explain well, because you should assume your client does not know what these technical topics mean.
+
+Notebook(s) with your analysis, which have comments in them for me to understand what you did.  (Use markdown.)
+
+Points:
+
+* 10 for following directions above.
+* 20 for good PPT/PDF report that is clear and contains summary of your results, graphs/charts, text excerpts, wordclouds..
+* 50 for use of techniques covered in class.  This means you did a lot of analysis types, tried a lot of things, applied what we learned.
+* 10 Good code in your notebook. Good spacing, clean variable names, use of functions when appropriate, variables are used if introduced, etc.
+* 10 Good comments in your notebook. This means using markdown, not just # comments.  Explain your process and results!
+
+Due on March 6.
+
+
+Twitter sources for Academic Data Analysis: https://gwu-libraries.github.io/sfm-ui/posts/2017-09-14-twitter-data
---
+++ week4/nlp_utilities.py
@@ -0,0 +1,116 @@
+
+import nltk
+from nltk.corpus import stopwords
+import re
+import string
+
+
+PUNCTUATION = string.punctuation
+STOPS = stopwords.words('english')
+
+def remove_punct(wordlist):
+    return [word for word in wordlist if word not in PUNCTUATION]
+
+
+def remove_stops(wordlist):
+    # takes a list of words and filters out stopwords after lowercasing
+    lowercase = [word.lower() for word in wordlist]
+    return [word for word in lowercase if word not in STOPS]
+
+
+def remove_nonletters(wordlist):
+    """ Checks for letters in the token - using a regex search.
+    Strings that are just numbers or remaining punctuation will 
+    be removed! No more custom '--'. But ''s' will remain.
+    """
+    return [word for word in wordlist if re.search('[a-zA-Z]', word)]
+
+
+def clean_tokens(tokens):
+    """ Takes a list of tokens. Returns lowercased, minus punct 
+    and stopwords and digits.
+    """
+    words = remove_punct(tokens)
+    words = remove_stops(words)
+    words = remove_nonletters(words)
+    return words
+
+
+def stem_tokens(tokens):
+    from nltk.stem.porter import PorterStemmer
+    stemmer = PorterStemmer()
+    return [stemmer.stem(word) for word in tokens]
+
+
+def print_counts(tokens, count=10):
+    # Takes a list of words, counts, prints top "count" words.
+    from collections import Counter
+    mycounts = Counter(tokens)
+    print("Word\tCount")
+    for word,count in mycounts.most_common(count):
+        print("%s\t%s" % (word,count))
+
+
+def tokenize_text(path):
+    # This takes a file path and word_tokenizes it, returning a list of words.
+    words = None
+    try:
+        with open(path, encoding="utf8", errors="ignore") as handle:
+            text = handle.read()
+            text = text.replace("\n", " ")
+            words = nltk.word_tokenize(text)
+            return words
+    except:
+        return None
+
+
+def tokenize_clean_stem(text):
+    # takes an already read text and tokenizes stems and clean
+    tokens = nltk.word_tokenize(text)
+    tokens = clean_tokens(tokens)
+    tokens = stem_tokens(tokens)
+    return tokens
+
+
+def tokenize_clean(text):
+    # takes an already read text and tokenizes stems and clean
+    tokens = nltk.word_tokenize(text)
+    tokens = clean_tokens(tokens)
+    return tokens
+
+
+def remove_custom(wordlist, mylist):
+    return [word for word in wordlist if word not in mylist]
+
+
+def count_sentences(text):
+    # Takes a full text, not tokens - because it tokenizes them.
+    # Returns the count only
+    return len(nltk.sent_tokenize(text))
+
+
+def get_filenames(folder):
+    from os import listdir
+    from os.path import isfile, join
+    # because we want to return full paths, we need to make sure there is
+    # a / at the end.
+    # If this doesn't work on Windows, change the slash direction.
+    if not folder.endswith("/"):
+        folder = folder + "/"
+    # this will return only the filenames, not folders inside the path
+    return [folder + f for f in listdir(folder) if isfile(join(folder, f)) and f != ".DS_Store"]
+
+def tokenize_dict(dict_of_file_text, tokenize=tokenize_clean):
+    """ Takes the output file-text dict from load_texts_as_strings and tokenizes using whichever function you want.
+    """
+    return
+
+def load_texts_as_string(filenames):
+    """ Input is a list, output is a dict """
+    from collections import defaultdict
+    loaded_text = defaultdict(str)  # each value is a string, the text
+    for filename in filenames:
+        with open(filename, errors="ignore") as handle:
+            loaded_text[filename] = handle.read()
+    return loaded_text
+
